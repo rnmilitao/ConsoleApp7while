212aaf1b209d77d7104459b6fe58d4e70d8a338e
Rozicleide <roziibr@hotmail.com>
2022-03-10 19:25:01 -0400

    Add project files.

---
+++ ConsoleApp7while/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            while (x <= 10)
+            {
+                Console.WriteLine(x);
+                x++;
+                Console.ReadKey();
+            }
+        }
+    }
+}
